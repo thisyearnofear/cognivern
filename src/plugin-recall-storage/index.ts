452a32cb8f77802dcf5c5aca6ac9cca164eb7cb1
Mark Krasner <mzkrasner@gmail.com>
2025-02-06 11:09:23 -0500

    Adding get account balance action to plugin

--- src/plugin-recall-storage/index.ts
+++ src/plugin-recall-storage/index.ts
@@ -2,11 +2,12 @@ import { Plugin } from "@elizaos/core";
 import { recallCotProvider } from "./providers/index.ts";
 import { RecallService } from "./services/recall.service.ts";
 import { buyCreditAction } from "./actions/buy-credit.ts";
+import { getCreditBalanceAction } from "./actions/get-balance.ts";
 
 export const recallStoragePlugin: Plugin = {
   name: "Recall Storage Plugin",
   description: "Provides basic Recall storage functionality",
-  actions: [buyCreditAction],
+  actions: [buyCreditAction, getCreditBalanceAction],
   //   evaluators: [knowledgeEvaluator],
   providers: [recallCotProvider],
   services: [RecallService.getInstance()],
