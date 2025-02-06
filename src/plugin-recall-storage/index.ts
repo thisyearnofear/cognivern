3813411e50f63f85b480e3a2624e0f707843b824
Mark Krasner <mzkrasner@gmail.com>
2025-02-06 13:50:44 -0500

    Adding a list buckets action

--- src/plugin-recall-storage/index.ts
+++ src/plugin-recall-storage/index.ts
@@ -3,12 +3,13 @@ import { recallCotProvider } from "./providers/index.ts";
 import { RecallService } from "./services/recall.service.ts";
 import { buyCreditAction } from "./actions/buy-credit.ts";
 import { getCreditBalanceAction } from "./actions/get-balance.ts";
-import {getAccountInfoAction} from "./actions/get-account.ts";
+import { getAccountInfoAction } from "./actions/get-account.ts";
+import { listBucketsAction } from "./actions/list-buckets.ts";
 
 export const recallStoragePlugin: Plugin = {
   name: "Recall Storage Plugin",
   description: "Provides basic Recall storage functionality",
-  actions: [buyCreditAction, getCreditBalanceAction, getAccountInfoAction],
+  actions: [buyCreditAction, getCreditBalanceAction, getAccountInfoAction, listBucketsAction],
   //   evaluators: [knowledgeEvaluator],
   providers: [recallCotProvider],
   services: [RecallService.getInstance()],
