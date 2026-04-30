4b3d4a09b8fe7ff8f1d1e46a8c96866baca84d92
Quincybob <74205675+Quincybob@users.noreply.github.com>
2026-04-30 17:04:53 +0300

    Add missing useMemo import to RunLedger

--- src/frontend/src/components/cre/RunLedger.tsx
+++ src/frontend/src/components/cre/RunLedger.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { css } from '@emotion/react';
 import { Link } from 'react-router-dom';
 import {
